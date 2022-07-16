d27f16b4692bcdff30fc595d86492b4aae7080d8
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-07-16 14:46:37 +0300

    Added states and showed its asynchronous aspect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
-
+  //Testing while using the call back in the set function
+  const [countDownTrial, setCountdownTrial] = useState(10)
   function increment() {
+    console.log(`Before setState : ${count}`)
     setCount(count + 1);
+    console.log(`After setState : ${count}`)
+  }
+  function decrement(){
+    console.log(`Before setState(Counting Downwards) : ${countDownTrial}`)
+    setCountdownTrial((countingdownwards) => countingdownwards - 1)
+    console.log(`After setState(Counting Downwards) : ${countDownTrial}`)
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <>
+  <button onClick={increment}>I have been clicked {count} times</button>;
+  <button onClick={decrement}>I am decrementing a number here: {countDownTrial}</button>
+  </>
 }
 
 export default Counter;
